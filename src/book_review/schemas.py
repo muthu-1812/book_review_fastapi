87d40bfc0fce42e9252c1c3432074ad20306b9d1
Muthukumaran M <muthukumaranmblaze99@gmail.com>
2024-03-27 19:12:55 +0530

    Minor bugfix

--- src/book_review/schemas.py
+++ src/book_review/schemas.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field, EmailStr
 class BookBase(BaseModel):
     title: str = Field(min_length=2, max_length=256, description="Title of the book.")
     author: str = Field(min_length=2, max_length=256, description="Author of the book")
-    publication_year: int = Field(gt=0, le=datetime.now().year,
+    publication_year: int = Field(ge=0, le=datetime.now().year,
                                   description="Year value should not be in future or less than zero")
 
 
@@ -24,7 +24,7 @@ class Book(BookBase):
 
 
 class ReviewBase(BaseModel):
-    ratings: float = Field(ge=0.0, le=5.0, decimal_places=2, description="Providing a rating between 0-5")
+    ratings: float = Field(ge=0, le=5.0, description="Providing a rating between 0-5")
     review: str = Field(min_length=10, description="Provide a review(min length:10 characters)")
     review_author: str = Field(min_length=3, description="Provide your name")
     reviewer_email: EmailStr = Field(description="Provide email id to send a notification")
